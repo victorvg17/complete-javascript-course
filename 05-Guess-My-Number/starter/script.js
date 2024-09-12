332df342ab054838a010494cda738aabb8c23681
victor <victor@psiori.com>
2024-09-12 07:50:05 +0200

    guess-my-number progress

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -15,6 +15,34 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+const margin = 5;
+
+// console.log(
+//   document.querySelector('.number').textContent,
+//   typeof document.querySelector('.number').textContent
+// );
+
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct number';
+    document.querySelector('.number').textContent = secretNumber;
+  } else if (guess > secretNumber) {
+    if (guess - secretNumber > margin) {
+      document.querySelector('.message').textContent = '📈 📈 Too high';
+    } else {
+      document.querySelector('.message').textContent = '📈 High';
+    }
+  } else {
+    if (secretNumber - guess > margin) {
+      document.querySelector('.message').textContent = '📉 📉 Too low';
+    } else {
+      document.querySelector('.message').textContent = '📉 Low';
+    }
+  }
 });
