ca81ad202f3fa1732df17a1824ec1407f5031320
victor <victor@psiori.com>
2024-09-23 21:24:44 +0200

    Guess my number finished, refactoring and high-score implemented

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -15,9 +15,10 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const upperLimit = 4;
+const upperLimit = 20;
 let secretNumber = Math.trunc(Math.random() * upperLimit) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.again').addEventListener('click', function () {
   console.log('Again clicked');
@@ -46,18 +47,16 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📈 📈 Too high';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
+
+    // update high score
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
     }
   } else {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📉 📉 Too low';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈 📈 Too high' : '📉 📉 Too low';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
